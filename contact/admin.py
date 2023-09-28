3dca68b858ae3f6755389caa2b52904179a04591
Tiago Rodrigues <yutri.trds@gmail.com>
2023-09-28 11:52:08 -0300

    Added header and ID urls

--- contact/admin.py
+++ contact/admin.py
@@ -4,13 +4,13 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
     ordering = '-id',
     # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 100
-    list_editable = 'first_name', 'last_name',
+    list_editable = 'first_name', 'last_name', 'show',
     list_display_links = 'phone', 'id'
 
 
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+from django.http import Http404
 
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .order_by('-id')[:10]
 
     context = {
         'contacts': contacts,
@@ -12,5 +15,25 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+
+    if single_contact is None:
+        raise Http404
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
