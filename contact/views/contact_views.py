88b1f1c93c5b0756d27cbf7dc61fcdbc23733926
Tiago Rodrigues <yutri.trds@gmail.com>
2023-09-28 16:46:58 -0300

    Search bar works on all fields

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
 
@@ -10,6 +11,35 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
     }
 
     return render(
@@ -25,11 +55,16 @@ def contact(request, contact_id):
         Contact, pk=contact_id, show=True
     )
 
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
     if single_contact is None:
         raise Http404
 
     context = {
         'contact': single_contact,
+        'site_title': site_title
+
+
     }
 
     return render(
