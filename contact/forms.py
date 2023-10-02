f15fbc4f5e72505e9a7f80600fc990cafbd4f853
Tiago Rodrigues <yutri.trds@gmail.com>
2023-10-02 08:06:50 -0300

    moved forms and cleaned imports

---
+++ contact/forms.py
@@ -0,0 +1,24 @@
+from django.core.exceptions import ValidationError
+from django import forms
+from . import models
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = models.Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+        )
+
+    def clean(self):
+        # cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+
+        return super().clean()
