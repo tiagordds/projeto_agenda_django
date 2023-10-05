90d3570f02b482d243fdf90021890d5edf6a2c7e
Tiago Rodrigues <yutri.trds@gmail.com>
2023-10-05 17:45:19 -0300

    added update

--- contact/urls.py
+++ contact/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     # contact (CRUD)
     path('contact/<int:contact_id>/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+
 ]
