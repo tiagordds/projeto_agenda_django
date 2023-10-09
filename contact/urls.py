c0d4b67e663b455982bb2e651d5204b25c70c73f
Tiago Rodrigues <yutri.trds@gmail.com>
2023-10-09 15:35:43 -0300

    added register, login and update users

--- contact/urls.py
+++ contact/urls.py
@@ -15,4 +15,10 @@ urlpatterns = [
     path('contact/<int:contact_id>/', views.contact, name='contact'),
     path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+
+    # user
+    path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
+    path('user/update/', views.user_update, name='user_update'),
 ]
--- contact/views/__init__.py
+++ contact/views/__init__.py
@@ -3,3 +3,4 @@
 
 from .contact_forms import *
 from .contact_views import *
+from .user_forms import *
